59e79007fe381a45cc1fc5d4dd38a7cf97a731e4
Eyoel W Feleke <feleke.e@northeastern.edu>
2022-02-22 13:00:24 -0800

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function toggleNavBar() {
+    var x = document.getElementById("NavLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
